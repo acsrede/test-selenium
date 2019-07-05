e5685d284da6392a4b706e2e757668601ed6350d
acsrede <acs.rede@gmail.com>
2019-07-05 15:51:08 -0300

    ajuste na classe de teste

--- src/test/java/roboselenium/ChromeTest.java
+++ src/test/java/roboselenium/ChromeTest.java
@@ -1,15 +1,17 @@
 package roboselenium;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeTest {
-	
+
 	@Test
 	public void LaunchChrome_Method1() {
-		
+
 		String _path = "src/main/resources/libs/chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", _path);
 
@@ -20,10 +22,23 @@ public class ChromeTest {
 		// Instantiate Web Driver
 		WebDriver driver = new ChromeDriver(options);
 		driver.get("http://www.google.com");
+
+		// Search on Google
+		driver.findElement(By.name("q")).sendKeys("selenium webdriver");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
 		System.out.println("Page title is - " + driver.getTitle());
-		
-		
-		
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// Display number of results on Console
+		System.out.println("Total Results - " + driver.findElement(By.id("resultStats")).getText());
+
+		driver.close();
+
 	}
 
 }
